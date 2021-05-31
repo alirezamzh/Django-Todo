a07dee23213f262bc7c894f0bd8b6ebb6da3f126
Alireza Memarzade <alireza.am885@gmail.com>
2021-05-31 11:44:21 +0430

    initial commit

---
+++ todos/views.py
@@ -0,0 +1,20 @@
+from django.db.models.query import QuerySet
+from rest_framework import generics
+
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class ListTodo(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+
+
+
+
